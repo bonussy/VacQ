ec3aac8d52285735f9f250fff822d624a7edb348
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-04-02 21:33:05 +0700

    add Swagger

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const helmet = require('helmet');
 const {xss} = require('express-xss-sanitizer');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+//Swagger
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Route files
 const hospitals = require('./routes/hospitals');
@@ -42,12 +45,32 @@ app.use(xss());
 //Rate Limiting
 const limiter = rateLimit({
     windowsMs:10*60*1000,//10 mins
-    max:1
+    max:100
 });
 app.use(limiter);
 
 //Prevent http param pollutions 
 app.use(hpp());
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+    };
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
